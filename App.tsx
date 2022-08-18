b35bf2508ce4e4c5681a2bc493845fefdfa7e0d6
sonalvi <sonal.vishwakarma@ascentfactory.com>
2022-08-18 17:34:40 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ClerkProvider, ClerkLoaded, useUser } from "@clerk/clerk-expo";
+import { tokenCache } from "./cache";
+import Navigation from './src/navigation'
+
+const CLERK_FRONT_END_API_KEY = "clerk.simple.sheepdog-97.lcl.dev";
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider frontendApi={CLERK_FRONT_END_API_KEY} tokenCache={tokenCache}>
+      <SafeAreaProvider>
+        <Navigation />
+        <StatusBar style="auto" />
+      </SafeAreaProvider>
+    </ClerkProvider>
   );
 }
 
